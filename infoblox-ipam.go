433ea4783c942b22ea479b4921139a4ef02ba9cb
shravindran <shravindran@infoblox.com>
2018-01-30 19:27:23 +0530

    Gateway creation with different format of gateway added

--- infoblox-ipam.go
+++ infoblox-ipam.go
@@ -33,11 +33,12 @@ type Container struct {
 
 type IBInfobloxDriver interface {
 	RequestNetworkView(netviewName string) (string, error)
-	RequestAddress(netviewName string, cidr string, ipAddr string, macAddr string, gw net.IP, vmID string) (string, error)
+	RequestAddress(netviewName string, cidr string, ipAddr string, macAddr string, vmID string) (string, error)
 	GetAddress(netviewName string, cidr string, ipAddr string, macAddr string) (*ibclient.FixedAddress, error)
 	UpdateAddress(fixedAddrRef string, macAddr string, vmID string) (*ibclient.FixedAddress, error)
 	ReleaseAddress(netviewName string, ipAddr string, macAddr string) (ref string, err error)
 	RequestNetwork(netconf NetConfig) (network string, err error)
+	CreateGateway(cidr string,gw net.IP,netviewName string)(string,error)
 }
 
 type InfobloxDriver struct {
@@ -72,26 +73,10 @@ func (ibDrv *InfobloxDriver) GetAddress(netviewName string, cidr string, ipAddr 
 	return fixedAddr, err
 }
 
-func (ibDrv *InfobloxDriver) RequestAddress(netviewName string, cidr string, ipAddr string, macAddr string, gw net.IP, vmID string) (string, error) {
+func (ibDrv *InfobloxDriver) RequestAddress(netviewName string, cidr string, ipAddr string, macAddr string, vmID string) (string, error) {
 	var fixedAddr *ibclient.FixedAddress
-	gateway := gw.String()
 	if netviewName == "" {
 		netviewName = ibDrv.DefaultNetworkView
-	}
-
-	//cni is not calling gateway creation call, so it is implemented here
-	//if gateway is not provided in net conf file by customer, it wont create as for now
-	//checking for gw ip already created ,if not creating
-	if gateway != "" {
-		gatewayIp, err := ibDrv.objMgr.GetFixedAddress(netviewName, cidr, gateway, "")
-		if err == nil && gatewayIp != nil {
-			log.Println("The Gateway already created")
-		} else if gatewayIp == nil {
-			_, err := ibDrv.objMgr.AllocateIP(netviewName, cidr, gateway, "", "")
-			if err != nil {
-				log.Printf("Gateway creation failed with error:'%s'", err)
-			}
-		}
 	}
 
 	if len(macAddr) == 0 {
@@ -263,6 +248,38 @@ func (ibDrv *InfobloxDriver) RequestNetwork(netconf NetConfig) (network string, 
 		network = ibNetwork.Cidr
 	}
 	return network, err
+}
+
+func (ibDrv *InfobloxDriver)CreateGateway(cidr string,gw net.IP,netviewName string)(string, error){
+
+	gw = gw.To4() //making sure it is only 4 bytes
+	//check for the format of gateway is in 0.0.0.x given by customer
+	//it happens when no subnet given in the conf file
+	if gw[0] ==0{
+		subnetIp,subnet,_:=net.ParseCIDR(cidr)
+		subnetIp = subnetIp.To4()
+		for index:=0;index<=3;index++{
+			if gw[index]==0{
+				gw[index]=subnetIp[index]
+			}
+		}
+		if subnet.Contains(gw)==false{
+			return "",errors.New("gateway given is invalid")
+
+		}
+	}
+	gateway := gw.String()
+	//checking for gw ip already created ,if not creating
+	gatewayIp, err := ibDrv.objMgr.GetFixedAddress(netviewName, cidr, gateway, "")
+	if err == nil && gatewayIp != nil {
+		log.Println("The Gateway already created")
+	} else if gatewayIp == nil {
+		gatewayIp, err = ibDrv.objMgr.AllocateIP(netviewName, cidr, gateway, "", "")
+		if err != nil {
+			log.Printf("Gateway creation failed with error:'%s'", err)
+		}
+	}
+	return fmt.Sprintf("%s", gatewayIp),nil
 }
 
 func makeContainers(containerList string) []Container {
