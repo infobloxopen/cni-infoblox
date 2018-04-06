d4d51649f051baa76ebfe143c0e26444b0ebddf5
ajeyhiremath <ahiremath@infoblox.com>
2018-04-06 15:23:03 +0530

    Validating CIDR at install-cni.sh

--- scripts/install-cni.sh
+++ scripts/install-cni.sh
@@ -1,6 +1,22 @@
-#!/bin/sh
-
+#! /bin/bash
 set -e -x;
+
+checksubnet(){
+	subnt=`jq '.ipam.subnet' /install/config/$CONF_FILE_NAME`
+    temp="${subnt%\"}"
+    temp="${temp#\"}"
+	if [ -z ` egrep '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$' <<< $temp` ]; then
+		echo "invalid CIDR mention in ${CONF_FILE_NAME}"
+		if [ $1 -eq 1 ]; then # exit the script
+			exit 255
+		fi
+		return 1
+	else
+		return 0		
+    fi
+}
+
+checksubnet 1
 
 if [ -w "/host/opt/cni/bin/" ]; then
     cp -f /install/bin/* /host/opt/cni/bin/;
@@ -16,12 +32,16 @@ fi;
 LTIME=`stat -c %Z /install/config/..data/$CONF_FILE_NAME`
 while true
 do
+    
    ATIME=`stat -c %Z /install/config/..data/$CONF_FILE_NAME`
 
    if [[ "$ATIME" != "$LTIME" ]]
-   then
-    cp -f /install/config/$CONF_FILE_NAME /host/etc/cni/net.d/;
-    LTIME=$ATIME
+   then       
+	    checksubnet 0        
+        if [ $? -eq 0 ]; then
+			cp -f /install/config/$CONF_FILE_NAME /host/etc/cni/net.d/;
+	        LTIME=$ATIME
+        fi
    fi
    sleep 5
 done
