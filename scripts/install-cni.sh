cacc6452e7000c2840c0c83eb020381c86e4d670
Ajey Hiremath <33475681+AjeyHiremath@users.noreply.github.com>
2018-04-10 15:48:23 +0530

    Validating CIDR of network conf file at install-cni.sh (#36)

--- scripts/install-cni.sh
+++ scripts/install-cni.sh
@@ -1,27 +1,53 @@
-#!/bin/sh
-
+#! /bin/bash
 set -e -x;
 
-if [ -w "/host/opt/cni/bin/" ]; then
-    cp -f /install/bin/* /host/opt/cni/bin/;
-    echo "Wrote CNI binaries to /host/opt/cni/bin/";
+CONF_FILE_CONTAINER_PATH="/install/config"
+CONF_FILE_HOST_PATH="/host/etc/cni/net.d/"
+BIN_FILE_CONTAINER_PATH="/install/bin"
+BIN_FILE_HOST_PATH="/host/opt/cni/bin/"
+CONF_FILE="${CONF_FILE_CONTAINER_PATH}/..data/${CONF_FILE_NAME}"
+
+# Validating CIDR from network configuration file
+checksubnet() {
+    SUBNET=$(cat ${CONF_FILE} | jq 'select(.ipam.subnet != null) | .ipam.subnet' | tr -d \")
+    # If 'subnet' key it self not provided in network conf
+    if [ -z "${SUBNET}" ] ; then
+        return 0
+    fi
+    if [ -z $(egrep '^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$' <<< $SUBNET) ]; then
+        echo "Invalid CIDR mentioned in the conf file ${CONF_FILE_NAME}"
+  	    if [ $1 -eq 1 ]; then # exit the script
+  	        exit 255
+  	    fi
+        return 1
+    fi
+    return 0
+}
+
+checksubnet 1
+
+if [ -w "${BIN_FILE_HOST_PATH}" ]; then
+    cp -f ${BIN_FILE_CONTAINER_PATH}/* ${BIN_FILE_HOST_PATH};
+    echo "Wrote Infoblox CNI binaries to ${BIN_FILE_HOST_PATH}";
 fi;
 
-if [ -w "/host/etc/cni/net.d/" ]; then
-    cp -f /install/config/* /host/etc/cni/net.d/;
-    echo "Wrote netconf to /host/etc/cni/net.d/";
+if [ -w "${CONF_FILE_HOST_PATH}" ]; then
+    cp -f ${CONF_FILE_CONTAINER_PATH}/* ${CONF_FILE_HOST_PATH};
+    echo "Wrote network conf to ${CONF_FILE_HOST_PATH}";
 fi;
 
-# Polls for file change at /install/config/..data/$CONF_FILE_NAME
-LTIME=`stat -c %Z /install/config/..data/$CONF_FILE_NAME`
+# Polls for file change at ${CONF_FILE}
+LAST_MODIFIED_TIME=$(stat -c %Z ${CONF_FILE})
 while true
-do
-   ATIME=`stat -c %Z /install/config/..data/$CONF_FILE_NAME`
-
-   if [[ "$ATIME" != "$LTIME" ]]
-   then
-    cp -f /install/config/$CONF_FILE_NAME /host/etc/cni/net.d/;
-    LTIME=$ATIME
-   fi
-   sleep 5
+do    
+    CURRENT_MODIFIED_TIME=$(stat -c %Z ${CONF_FILE})
+    if [[ "$CURRENT_MODIFIED_TIME" != "$LAST_MODIFIED_TIME" ]]; then	    
+        checksubnet 0
+        if [ $? -eq 0 ]; then
+            echo "Network conf is modified so changing..."
+            cp -f ${CONF_FILE_CONTAINER_PATH}/${CONF_FILE_NAME} ${CONF_FILE_HOST_PATH};
+            LAST_MODIFIED_TIME=$CURRENT_MODIFIED_TIME
+        fi
+    fi
+    sleep 30
 done
